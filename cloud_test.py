4e39c97e88a7db006f439a70f4f9154054875726
Leonid <cernezenkol@gmail.com>
2026-04-02 14:26:39 +0000

    Добавил инфо о системе

--- cloud_test.py
+++ cloud_test.py
@@ -7,6 +7,9 @@ node = platform.node()
 
 print(f"привет, {name}!")
 
-print(f "Ты работаешь на системе: {system} , имя узла: {node}")
+print(f"Ты работаешь на системе: {system} , имя узла: {node}")
 
-print(f "Алена крутая девка: {system}, имя узла: {node}")
+print(f"Алена крутая девка: {system}, имя узла: {node}")
+
+print(f"Стас может мы не будем так долго разговаривать: {system}, имя узла: {node}")
+
