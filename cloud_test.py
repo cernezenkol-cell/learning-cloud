49948020a952ee1d7c8c7fd59eb9e60868c33037
cernezenkol-cell <cernezenkol@gmail.com>
2026-06-19 00:09:05 +0300

    Update cloud_test.py

--- cloud_test.py
+++ cloud_test.py
@@ -8,8 +8,3 @@ node = platform.node()
 print(f"привет, {name}!")
 
 print(f"Ты работаешь на системе: {system} , имя узла: {node}")
-
-print(f"Алена крутая девка: {system}, имя узла: {node}")
-
-print(f"Стас может мы не будем так долго разговаривать: {system}, имя узла: {node}")
-
